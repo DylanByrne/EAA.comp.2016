a62337a170df270d76e43659aae121e3841a6d46
Dylan Byrne <xxdylanbyrnexx@gmail.com>
2016-10-07 10:27:54 +0100

    push

--- week3/newscript.sh
+++ week3/newscript.sh
@@ -1,22 +1,3 @@
 #!/bin/bash
 
-echo "This is a script to create directories"
-
-#delete directory first
-rmdir week2
-
-#throws exception if exists
-mkdir week2
-
-#create 3 sub directories
-
-mkdir week2/subdir4
-
-#change directory
-cd week2
-
-mkdir one
-
-mkdir two
-
-mkdir three
+echo "Three commands entered"
