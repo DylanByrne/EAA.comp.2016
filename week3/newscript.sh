ca117c9611cfedabb7eba0b702ccbfa7a46168ed
Dylan Byrne <xxdylanbyrnexx@gmail.com>
2016-10-07 10:56:15 +0100

    push

--- week3/newscript.sh
+++ week3/newscript.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo "Three commands entered"
+#Paramters passing in are shown as $1 $2 $3
+
+echo "$# commands entered"
+echo "$1, $2, $3 is this correct"
+
+echo "Time and date" date +%d-%m-%y-%T
+
