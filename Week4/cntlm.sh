e6b481daf488ebd8146f8737fe54aab85655c169
Dylan Byrne <xxdylanbyrnexx@gmail.com>
2016-10-14 10:31:06 +0100

    Commit

---
+++ Week4/cntlm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ps -ef | grep cntlm | grep root
+
+#ps - report a snapshot of the current processes.
+#ps -ef - displays information on all running processes
+
+#grep cntlm searches for the word cntlm
+
+#this new command lists all the running process
+#and searches for the process that contains cntlm string
