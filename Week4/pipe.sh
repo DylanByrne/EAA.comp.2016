8961941f5aaf24283c6d6094faf3045fcc0a9765
Dylan Byrne <xxdylanbyrnexx@gmail.com>
2016-10-14 10:48:07 +0100

    Commit changes

--- Week4/pipe.sh
+++ Week4/pipe.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 ps -ef | grep mysql
+
+#ps - report a snapshot of the current processes.
+#ps -ef - displays information on all running processes
+
+#grep mysql searches for the word mysql
+
+#this new command lists all the running process
+#and searches for the process that contains mysql string
